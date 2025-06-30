ecc49c94ad5e47cf644d383cdc733c7d1ed1970e
Carlos Eduardo Hilario Ferreira <carloseduardohferreira@gmail.com>
2025-06-30 13:43:35 -0300

    Inicialmente implementada como uma APIView básica, a view foi refatorada para usar a classe genérica generics.ListAPIView e atualizamos o README.MD

--- apps/documentation/views.py
+++ apps/documentation/views.py
@@ -1,39 +1,35 @@
-# apps/documentation/views.py
-
 from django.db.models import Q
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics 
 
 from .models import DocumentChunk
 from .serializers import DocumentChunkSerializer
 
-class SearchAPIView(APIView):
+# A nossa view agora herda de generics.ListAPIView
+class SearchAPIView(generics.ListAPIView):
     """
     View da API para buscar nos fragmentos da documentação.
+    Usa o padrão de ListAPIView do DRF para uma implementação mais limpa.
     Aceita uma query via parâmetro GET 'q'.
-    Ex: /api/search/?q=sua_busca
+    Ex: /api/v1/search/?q=sua_busca
     """
-    def get(self, request, *args, **kwargs):
-        
+    # Definimos o serializer como um atributo da classe.
+    serializer_class = DocumentChunkSerializer
+
+    def get_queryset(self):
+        """
+        Este método é sobrescrito para controlar o conjunto de dados (queryset)
+        que a view irá retornar. A lógica de busca fica toda aqui.
+        """
         # 1. Pega o termo de busca dos parâmetros da URL
-        query = request.query_params.get('q', None)
+        query = self.request.query_params.get('q', None)
 
-        if not query:
-            # Se nenhuma busca for fornecida, retorna um erro amigável.
-            return Response(
-                {"error": "O parâmetro de busca 'q' é obrigatório. Exemplo de URL: ...search/?q=model"},
-                status=status.HTTP_400_BAD_REQUEST
+        # 2. Se uma query foi fornecida, filtra o banco de dados.
+        if query:
+            # Verifica se a busca não retornou none
+            return DocumentChunk.objects.filter(
+                Q(title__icontains=query) | Q(content__icontains=query)
             )
-
-        # 2. Faz a busca no banco de dados (case-insensitive)
-        # Busca no título OU no conteúdo.
-        search_results = DocumentChunk.objects.filter(
-            Q(title__icontains=query) | Q(content__icontains=query)
-        )
-
-        # 3. Usa o Serializer para "traduzir" os resultados
-        serializer = DocumentChunkSerializer(search_results, many=True)
-
-        # 4. Retorna a resposta em JSON
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        # 3. Aqui a listview trata a query vazia caso nenhuma query foi fornecida, retorna um queryset vazio.
+        # A ListAPIView vai lidar com isso e retornar uma lista vazia `[]` 
+        return DocumentChunk.objects.none()
